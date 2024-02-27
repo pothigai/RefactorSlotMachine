27fd245197db2822a57aab89ea66b132740a10c8
Pothigai <pothigaiselvan@gmail.com>
2024-02-28 00:03:46 +0100

    Added UI class.

--- Program.cs
+++ Program.cs
@@ -17,54 +17,57 @@
 
             Random rnd = new Random();
 
+            UISlotMachine UI = new UISlotMachine();
+
             //Ask the user for a min buy in of 500 points
-            Console.WriteLine($"Please enter your buy in amount, the minimum is {MIN_BUYIN} points: ");
-            string playerInput = Console.ReadLine();
+            UI.print($"Please enter your buy in amount, the minimum is {MIN_BUYIN} points:");
+
+            string playerInput = UI.scan();
             int totalPoints;
 
             while (!Int32.TryParse(playerInput, out totalPoints) || totalPoints < MIN_BUYIN)
             {
-                Console.WriteLine($"The entered amount either does not meet the minimum buy in amount or is not a valid input, please enter a valid amount of aleast {MIN_BUYIN}:");
-                playerInput = Console.ReadLine();
+                UI.print($"The entered amount either does not meet the minimum buy in amount or is not a valid input, please enter a valid amount of aleast {MIN_BUYIN}:");
+                playerInput = UI.scan();
             }
 
-            Console.WriteLine("Choose which lines to play (R = Row, C = Column, D = Diagonal) and then press P to play:");
+            UI.print("Choose which lines to play (R = Row, C = Column, D = Diagonal) and then press P to play:");
 
 
             bool[] selectedLines = new bool[3];
 
             while (true)
             {
-                string choice = Console.ReadLine().ToLower();
-                Console.WriteLine();
+                string choice = UI.scan().ToLower();
+                UI.print("");
 
                 if (choice == "r")
                 {
                     selectedLines[0] = true;
-                    Console.WriteLine("Row selected.");
+                    UI.print("Row selected.");
                 }
                 if (choice == "c")
                 {
                     selectedLines[1] = true;
-                    Console.WriteLine("Column selected.");
+                    UI.print("Column selected.");
                 }
                 if (choice == "d")
                 {
                     selectedLines[2] = true;
-                    Console.WriteLine("Diagonal selected.");
+                    UI.print("Diagonal selected.");
                 }
                 if (choice == "p")
                 {
 
-                    Console.WriteLine("Selected lines:");
-                    if (selectedLines[0]) Console.WriteLine("Row");
-                    if (selectedLines[1]) Console.WriteLine("Column");
-                    if (selectedLines[2]) Console.WriteLine("Diagonal");
+                    UI.print("Selected lines:");
+                    if (selectedLines[0]) UI.print("Row");
+                    if (selectedLines[1]) UI.print("Column");
+                    if (selectedLines[2]) UI.print("Diagonal");
                     break;
                 }
-                else
+                if (choice != "p" || choice != "r" || choice != "c" || choice != "d")
                 {
-                    Console.WriteLine("Invalid choice. Please select (R), (C), (D), or (P) to play.");
+                    UI.print("Invalid choice. Please select (R), (C), (D), or (P) to play.");
                 }
             }
 
@@ -72,24 +75,24 @@
 
             while (playAgain == POSITVE_INPUT)
             {
-                Console.Clear();
+                UI.clear();
 
                 //Checking if user has enough points to play
                 if (totalPoints <= 0)
                 {
-                    Console.WriteLine($"You do not have enough points, would you like to top up? ({POSITVE_INPUT}/{NEGATIVE_INPUT}): ");
-                    string topUp = Console.ReadLine();
+                    UI.print($"You do not have enough points, would you like to top up? ({POSITVE_INPUT}/{NEGATIVE_INPUT}): ");
+                    string topUp = UI.scan();
 
                     while (topUp.ToLower() != POSITVE_INPUT && topUp.ToLower() != NEGATIVE_INPUT)
                     {
-                        Console.Write($"Invalid input, please enter {POSITVE_INPUT}/{NEGATIVE_INPUT}:");
-                        topUp = Console.ReadLine();
+                        UI.print($"Invalid input, please enter {POSITVE_INPUT}/{NEGATIVE_INPUT}:");
+                        topUp = UI.scan();
                     }
 
                     if (topUp.ToLower() == POSITVE_INPUT)
                     {
-                        Console.WriteLine("Please enter the amount of points you want to purchase");
-                        totalPoints += Convert.ToInt32(Console.ReadLine());
+                        UI.print("Please enter the amount of points you want to purchase");
+                        totalPoints += Convert.ToInt32(UI.scan());
                     }
                     else
                     {
@@ -116,7 +119,7 @@
                 //Print the matrix to display to the user
                 for (int i = 0; i < MATRIX_SIZE; i++)
                 {
-                    Console.WriteLine("");
+                    UI.print("");
 
                     for (int j = 0; j < MATRIX_SIZE; j++)
                     {
@@ -124,11 +127,11 @@
                     }
                 }
 
-                Console.WriteLine("");
-                Console.WriteLine("Selected lines:");
-                if (selectedLines[0]) Console.WriteLine("Row");
-                if (selectedLines[1]) Console.WriteLine("Column");
-                if (selectedLines[2]) Console.WriteLine("Diagonal");
+                UI.print("");
+                UI.print("Selected lines:");
+                if (selectedLines[0]) UI.print("Row");
+                if (selectedLines[1]) UI.print("Column");
+                if (selectedLines[2]) UI.print("Diagonal");
 
 
                 bool rowMatch = false;
@@ -141,7 +144,7 @@
                     int rowCount;
                     if (rowCheck(slots, out rowCount))
                     {
-                        Console.WriteLine($"You won, all values in row {rowCount} are a match");
+                        UI.print($"You won, all values in row {rowCount} are a match");
                         totalPoints += HOR_POINT;
                         rowMatch = true;
                     }
@@ -153,13 +156,13 @@
                     int diagCount;
                     if (diagonalCheck(slots, out diagCount)[0])
                     {
-                        Console.WriteLine($"You won, all values in diagonal {diagCount} are a match");
+                        UI.print($"You won, all values in diagonal {diagCount} are a match");
                         totalPoints += DIAG_POINT;
                         diagonalMatch = true;
                     }
                     if (diagonalCheck(slots, out diagCount)[1])
                     {
-                        Console.WriteLine($"You won, all values in diagonal {diagCount} are a match");
+                        UI.print($"You won, all values in diagonal {diagCount} are a match");
                         totalPoints += DIAG_POINT;
                         diagonalMatch = true;
                     }
@@ -185,23 +188,23 @@
 
                 if (allValuesMatch)
                 {
-                    Console.WriteLine($"You won {JACKPOT} points, all values in the slot match!");
+                    UI.print($"You won {JACKPOT} points, all values in the slot match!");
                     totalPoints = totalPoints + JACKPOT;
                 }
                 if (!rowMatch && !diagonalMatch && !allValuesMatch)
                 {
-                    Console.WriteLine("You lost!");
+                    UI.print("You lost!");
                 }
 
                 //Display round winnings and ask user if they wish to play again
-                Console.WriteLine($"Your total points are {totalPoints}");
-                Console.WriteLine($"Do you want to play again? {POSITVE_INPUT}/{NEGATIVE_INPUT}");
-                playAgain = Console.ReadLine();
+                UI.print($"Your total points are {totalPoints}");
+                UI.print($"Do you want to play again? {POSITVE_INPUT}/{NEGATIVE_INPUT}");
+                playAgain = UI.scan();
 
                 while (playAgain.ToLower() != POSITVE_INPUT && playAgain.ToLower() != NEGATIVE_INPUT)
                 {
-                    Console.WriteLine($"Invalid input. Please enter '{POSITVE_INPUT}' or '{NEGATIVE_INPUT}'.");
-                    playAgain = Console.ReadLine();
+                    UI.print($"Invalid input. Please enter '{POSITVE_INPUT}' or '{NEGATIVE_INPUT}'.");
+                    playAgain = UI.scan();
                 }
             }
         }
---
+++ UISlotMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorSlotMachine
+{
+    internal class UISlotMachine
+    {
+        public void print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string scan()
+        {
+            string message = Console.ReadLine();
+            return message;
+        }
+
+        public void clear()
+        {
+            Console.Clear();
+        }
+    }
+}
